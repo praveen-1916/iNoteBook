618be7f5cd344f67051d76ac0932497cee747422
praveen-1916 <pkumarbattu@gmail.com>
2024-09-22 22:38:13 +0530

    Removing Backend

--- backend/databse.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const mongoURI = 'mongodb+srv://praveen1619:7732038@mycluster.1g30k.mongodb.net/?';
-
-const connectToMongo = async () => {
-    await mongoose.connect(mongoURI);
-}
-
-module.exports = connectToMongo;
