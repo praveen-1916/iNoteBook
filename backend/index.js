c0d0505d4f38077b90f78f09258ff6fd0b93f2fb
praveen-1916 <pkumarbattu@gmail.com>
2024-09-22 22:23:18 +0530

    Final code

--- backend/index.js
+++ backend/index.js
@@ -4,7 +4,7 @@ const cors = require('cors')
 
 connectToMongo();
 const app = express();
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
